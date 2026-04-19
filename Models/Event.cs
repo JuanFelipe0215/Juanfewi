5f269c987e0f08e3a272ac422844376f09e65c84
Juan Felipe Cardona Rios <juanfe13q@gmail.com>
2026-04-18 22:43:56 -0500

    fix: cambios model

--- Models/Event.cs
+++ Models/Event.cs
@@ -7,19 +7,16 @@ using System.ComponentModel.DataAnnotations;
 public class Event
 {
     public int Id { get; set; }
-
-    [Required]
-    [MaxLength(100)]
+    
     public string Name { get; set; }
 
-    [Required]
+
     public string Category { get; set; }
 
-    [Required]
+
     public DateTime Date { get; set; }
 
-    [Required]
-    [MaxLength(500)]
+
     public string Description { get; set; }
 
     public string Location { get; set; }
